8552a87bc6a968f7160a984dc092ad8d35d2cdb5
Rafal Rytel <rflrytel@gmail.com>
2021-06-12 18:54:23 +0200

    exceptions handling

--- src/com/company/FigureCreator.java
+++ src/com/company/FigureCreator.java
@@ -13,40 +13,45 @@ public class FigureCreator {
             case "koło":
                 return createCircle();
         }
-
         throw new IllegalArgumentException("Taka nazwa figury nie istnieje");
     }
 
     public static Figure createSquare() {
-        System.out.println("Podaj współrzędne rogów we właściwej kolejności x1 y1 x2 y2 x3 y3 x4 y4: ");
+        Scanner scanner = new Scanner(System.in);
 
-        return new Square(new Point(validatedDouble(), validatedDouble()),
-                new Point(validatedDouble(), validatedDouble()),
-                new Point(validatedDouble(), validatedDouble()),
-                new Point(validatedDouble(), validatedDouble()));
+        System.out.println("Podaj współrzędne rogów we właściwej kolejności x1 y1 x2 y2 x3 y3 x4 y4: ");
+        try {
+            return new Square(new Point(scanner.nextDouble(), scanner.nextDouble()),
+                    new Point(scanner.nextDouble(), scanner.nextDouble()),
+                    new Point(scanner.nextDouble(), scanner.nextDouble()),
+                    new Point(scanner.nextDouble(), scanner.nextDouble()));
+        } catch (Exception e) {
+            throw new IllegalArgumentException("Niepoprawne dane (współrzędna)");
+        }
     }
 
     public static Figure createTriangle() {
+        Scanner scanner = new Scanner(System.in);
         System.out.println("Podaj współrzędne rogów x1 y1 x2 y2 x3 y3: ");
-        return new Triangle(new Point(validatedDouble(), validatedDouble()),
-                new Point(validatedDouble(), validatedDouble()),
-                new Point(validatedDouble(), validatedDouble()));
+        try {
+            return new Triangle(new Point(scanner.nextDouble(), scanner.nextDouble()),
+                    new Point(scanner.nextDouble(), scanner.nextDouble()),
+                    new Point(scanner.nextDouble(), scanner.nextDouble()));
+        } catch (Exception e) {
+            throw new IllegalArgumentException("Niepoprawne dane (współrzędna)");
+        }
     }
 
     public static Figure createCircle() {
-        System.out.println("Podaj współrzędne środka koła oraz współrzędne punktu znajdującego sie na krawędzi koła x1 y1 x2 y2: ");
-        return new Circle(new Point(validatedDouble(), validatedDouble()),
-                new Point(validatedDouble(), validatedDouble()));
-    }
-
-    private static double validatedDouble() {
         Scanner scanner = new Scanner(System.in);
-        while (true) {
-            try {
-                return Double.parseDouble(scanner.next());
-            } catch (NumberFormatException ignore) {
-                System.out.println("Wpisano niewłaściwe dane");
-            }
+        System.out.println("Podaj współrzędne środka koła oraz współrzędne punktu znajdującego sie na krawędzi koła x1 y1 x2 y2: ");
+        try {
+            return new Circle(new Point(scanner.nextDouble(), scanner.nextDouble()),
+                    new Point(scanner.nextDouble(), scanner.nextDouble()));
+        } catch (Exception e) {
+            throw new IllegalArgumentException("Niepoprawne dane (współrzędna)");
         }
     }
 }
+
+
--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -7,26 +7,29 @@ public class Main {
     public static void main(String[] args) {
         while (true) {
             Scanner scanner = new Scanner(System.in);
+            try {
+                System.out.println("podaj nazwę figury: kwadrat, trójkąt, koło. 0 wyjście");
+                String figureName = scanner.next();
 
-            System.out.println("podaj nazwę figury: kwadrat, trójkąt, koło. 0 wyjście");
-            String figureName = scanner.next();
-
-            if (figureName.equals("0")) {
-                return;
-            }
-            System.out.println("pole czy obwód?");
-            String areaOrPerimeter = scanner.next();
-
-            Figure figure = FigureCreator.getFigure(figureName);
-
-            if (figure.isCorrect()) {
-                if (areaOrPerimeter.equals("pole")) {
-                    System.out.println("Pole figury " + figure.getName() + " wynosi " + OperatorHelper.calcArea(figure));
-                } else if (areaOrPerimeter.equals("obwód")) {
-                    System.out.println("Obwód figury " + figure.getName() + " wynosi " + OperatorHelper.calcPerimeter(figure));
-                } else {
-                    System.out.println("Nie wpisano poprawnie obwód/pole");
+                if (figureName.equals("0")) {
+                    return;
                 }
+                System.out.println("pole czy obwód?");
+                String areaOrPerimeter = scanner.next();
+
+                Figure figure = FigureCreator.getFigure(figureName);
+
+                if (figure.isCorrect()) {
+                    if (areaOrPerimeter.equals("pole")) {
+                        System.out.println("Pole figury " + figure.getName() + " wynosi " + OperatorHelper.calcArea(figure));
+                    } else if (areaOrPerimeter.equals("obwód")) {
+                        System.out.println("Obwód figury " + figure.getName() + " wynosi " + OperatorHelper.calcPerimeter(figure));
+                    } else {
+                        System.out.println("Nie wpisano poprawnie obwód/pole");
+                    }
+                }
+            } catch (Exception e) {
+                System.out.println(e);
             }
         }
     }
